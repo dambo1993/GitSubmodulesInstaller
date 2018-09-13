b75a0dbabc3c64886915a2979fcdc2ce3f024080
przemyslaw.w.michalak <przemyslaw.w.michalak@student.put.poznan.pl>
2018-09-13 19:56:53 +0200

    update my submodules list

--- GitSubmodulesInstaller/Form1.Designer.cs
+++ GitSubmodulesInstaller/Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.b_load_file = new System.Windows.Forms.Button();
-            this.gb_submodules = new System.Windows.Forms.GroupBox();
             this.tv_XML = new System.Windows.Forms.TreeView();
+            this.b_generate_command = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // b_load_file
             // 
-            this.b_load_file.Location = new System.Drawing.Point(239, 12);
+            this.b_load_file.Location = new System.Drawing.Point(12, 344);
             this.b_load_file.Name = "b_load_file";
             this.b_load_file.Size = new System.Drawing.Size(130, 23);
             this.b_load_file.TabIndex = 0;
@@ -43,32 +43,34 @@
             this.b_load_file.UseVisualStyleBackColor = true;
             this.b_load_file.Click += new System.EventHandler(this.b_load_file_Click);
             // 
-            // gb_submodules
-            // 
-            this.gb_submodules.Location = new System.Drawing.Point(12, 12);
-            this.gb_submodules.Name = "gb_submodules";
-            this.gb_submodules.Size = new System.Drawing.Size(200, 100);
-            this.gb_submodules.TabIndex = 1;
-            this.gb_submodules.TabStop = false;
-            this.gb_submodules.Text = "Submodules";
-            // 
             // tv_XML
             // 
-            this.tv_XML.Location = new System.Drawing.Point(107, 203);
+            this.tv_XML.Location = new System.Drawing.Point(12, 12);
             this.tv_XML.Name = "tv_XML";
-            this.tv_XML.Size = new System.Drawing.Size(262, 136);
+            this.tv_XML.Size = new System.Drawing.Size(262, 326);
             this.tv_XML.TabIndex = 2;
+            // 
+            // b_generate_command
+            // 
+            this.b_generate_command.Location = new System.Drawing.Point(12, 373);
+            this.b_generate_command.Name = "b_generate_command";
+            this.b_generate_command.Size = new System.Drawing.Size(130, 23);
+            this.b_generate_command.TabIndex = 3;
+            this.b_generate_command.Text = "Generate Git command";
+            this.b_generate_command.UseVisualStyleBackColor = true;
+            this.b_generate_command.Click += new System.EventHandler(this.b_generate_command_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(286, 450);
+            this.Controls.Add(this.b_generate_command);
             this.Controls.Add(this.tv_XML);
-            this.Controls.Add(this.gb_submodules);
             this.Controls.Add(this.b_load_file);
             this.Name = "Form1";
             this.Text = "GitSubmodulesInstaller";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -76,8 +78,8 @@
         #endregion
 
         private System.Windows.Forms.Button b_load_file;
-        private System.Windows.Forms.GroupBox gb_submodules;
         private System.Windows.Forms.TreeView tv_XML;
+        private System.Windows.Forms.Button b_generate_command;
     }
 }
 
--- GitSubmodulesInstaller/Form1.cs
+++ GitSubmodulesInstaller/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,20 +13,11 @@ using System.Xml;
 
 // XML editor online: https://www.tutorialspoint.com/online_xml_editor.htm
 // XML C# tutorial: https://4programmers.net/C_sharp/Wprowadzenie/Rozdzia%C5%82_13
-
 namespace GitSubmodulesInstaller
 {
     public partial class Form1 : Form
     {
-
-        XmlDocument XmlDoc = new XmlDocument();
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void b_load_file_Click(object sender, EventArgs e)
+        private void ui_load_xml_file(string path)
         {
             TreeNode NewXmlNode;
 
@@ -34,23 +26,132 @@ namespace GitSubmodulesInstaller
 
             try
             {
-                XmlDoc.Load("mySubmodules.xml");
+                XmlDoc.Load(path);
                 int Count = XmlDoc.GetElementsByTagName("Submodule").Count;
 
                 for (int i = 0; i < Count; i++)
                 {
                     NewXmlNode = tv_XML.Nodes.Add(XmlDoc.GetElementsByTagName("Name").Item(i).InnerText);
 
-                    NewXmlNode.Nodes.Add("URL: " +
+                    NewXmlNode.Nodes.Add(
                         XmlDoc.GetElementsByTagName("URL").Item(i).InnerText);
                     NewXmlNode.Nodes.Add("Description: " +
                         XmlDoc.GetElementsByTagName("Description").Item(i).InnerText);
+                    NewXmlNode.Nodes.Add(
+                        XmlDoc.GetElementsByTagName("Path").Item(i).InnerText);
                 }
             }
             catch (XmlException ex)
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private string nodeTextSeparate(String text)
+        {
+            string[] array = text.Split(':');
+
+            return array[1].Trim();
+        }
+
+        private string generate_single_submodule_command(TreeNode node)
+        {
+            string command = "git submodule add ";
+
+            try
+            {
+                string url = node.Nodes[0].Text;
+
+                string path = node.Nodes[2].Text;
+                
+                command += url + " " + path;
+            }
+            catch
+            {
+
+            }
+
+            return command;
+        }
+
+        private string ui_generate_git_command()
+        {
+            string command = "";
+
+            foreach(TreeNode node in tv_XML.Nodes)
+            {
+                Console.WriteLine(node.Text); // test - only main nodes
+                if(node.Checked)
+                {
+                    command += generate_single_submodule_command(node) + Environment.NewLine;
+                }
+            }
+            Console.WriteLine(command);
+
+            return command;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tv_XML.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Bottom | AnchorStyles.Right;
+
+            tv_XML.CheckBoxes = true;
+
+            tv_XML.DrawMode = TreeViewDrawMode.OwnerDrawAll;
+            tv_XML.DrawNode += xmlStructureTree_DrawNode;
+        }
+
+        // disable checkboxes for children nodes in TV
+        private void xmlStructureTree_DrawNode(object sender, DrawTreeNodeEventArgs e)
+        {
+            if (e.Node.Nodes.Count == 0)
+            {
+                e.Node.Checked = false;
+            }
+            e.DrawDefault = true;
+        }
+
+        private void b_load_file_Click(object sender, EventArgs e)
+        {
+            ui_load_xml_file("mySubmodules.xml");
+        }
+
+        protected void ClipboardSetText(string inTextToCopy)
+        {
+            var clipboardThread = new Thread(() => ClipBoardThreadWorker(inTextToCopy));
+            clipboardThread.SetApartmentState(ApartmentState.STA);
+            clipboardThread.IsBackground = false;
+            clipboardThread.Start();
+        }
+
+        private void ClipBoardThreadWorker(string inTextToCopy)
+        {
+            System.Windows.Forms.Clipboard.SetText(inTextToCopy);
+        }
+
+        private void b_generate_command_Click(object sender, EventArgs e)
+        {
+            string command = ui_generate_git_command();
+            if(command != "")
+            {
+                MessageBox.Show("Your submodules init command:" + Environment.NewLine +
+                command +
+                "Copied to Your clipboard"
+                );
+
+                // from https://stackoverflow.com/questions/930219/how-to-handle-blocked-clipboard-and-other-oddities
+                Clipboard.SetDataObject(
+                    command,     //text to store in clipboard
+                    false,       //do not keep after our app exits
+                    5,           //retry 5 times
+                    200);        //200ms delay between retries
+            }            
         }
     }
 }
